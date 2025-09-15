91193dfb5aef807070ec9baf57ced4c0edfb65dc
AkseliHyge <hytonenakseli@gmail.com>
2025-09-15 22:23:05 +0300

    eka

---
+++ js/basic.js
@@ -0,0 +1,8 @@
+event.preventDefault();
+
+const form = document.getElementById("user-form");
+const usernameInput = document.getElementById("input-username");
+const emailInput = document.getElementById("input-email");
+const adminInput = document.getElementById("input-admin");
+const tableBody = document.getElementById("table-body");
+const emptyButton = document.getElementById("empty-table");
