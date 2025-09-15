cc198be7b27a6093bff221234fe97f904d582c60
AkseliHyge <hytonenakseli@gmail.com>
2025-09-15 23:19:21 +0300

    neljäs

--- js/basic.js
+++ js/basic.js
@@ -1,8 +1,34 @@
-event.preventDefault();
+console.log("koodi pyörii");
 
-const form = document.getElementById("user-form");
-const usernameInput = document.getElementById("input-username");
-const emailInput = document.getElementById("input-email");
-const adminInput = document.getElementById("input-admin");
-const tableBody = document.getElementById("table-body");
-const emptyButton = document.getElementById("empty-table");
+const addDataButton = document.getElementById("submit-data");
+const emptyTableButton = document.getElementById("empty-table");
+
+addDataButton.addEventListener("click", function() {
+    const username = document.getElementById("input-username").value;
+    const email = document.getElementById("input-email").value;
+    const admin = document.getElementById("input-admin").checked ? "X" : "-";
+
+    const table = document.querySelector("table");
+
+    let trNew = document.createElement("tr");
+    let newName = document.createElement("td");
+    let NewEmail = document.createElement("td");
+    let NewAdmin = document.createElement("td");
+
+    newName.textContent = username;
+    NewEmail.textContent = email;
+    NewAdmin.textContent = admin;
+
+    trNew.appendChild(newName);
+    trNew.appendChild(NewEmail);
+    trNew.appendChild(NewAdmin);
+
+    table.appendChild(trNew);
+
+    
+});
+
+emptyTableButton.addEventListener("click", function() {
+    const tableBody = document.getElementById("user-table");
+    tableBody.innerHTML = '';
+});
